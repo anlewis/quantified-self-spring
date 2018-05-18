0e4bb864bcbd78351bb90276c423c9caf90013e4
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-17 19:43:41 -0600

    add get food by id endpoint

--- src/main/java/com/quantifiedselfspring/WebController.java
+++ src/main/java/com/quantifiedselfspring/WebController.java
@@ -1,9 +1,11 @@
 package com.quantifiedselfspring;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +23,18 @@ public class WebController{
     public List<Food> retrieveAllStudents() {
         return foodRepository.findAll();
     }
+
+    @GetMapping("/foods/{id}")
+    public Food retrieveStudent(@PathVariable long id) {
+        Optional<Food> food = foodRepository.findById(id);
+
+//        if (!student.isPresent())
+//            throw new FoodNotFoundException("id-" + id);
+
+        return food.get();
+    }
 }
+
+
+
+
